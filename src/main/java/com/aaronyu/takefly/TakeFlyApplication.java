8d8dc9c4ddec0a9898b0ffbf5b8b086cd0652632
AaronYu <756249753@qq.com>
2022-09-08 10:14:01 +0800

    init project

---
+++ src/main/java/com/aaronyu/takefly/TakeFlyApplication.java
@@ -0,0 +1,13 @@
+package com.aaronyu.takefly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeFlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeFlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aaronyu/takefly/TakeFlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaronyu.takefly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeFlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
